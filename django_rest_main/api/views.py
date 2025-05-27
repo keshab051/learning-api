73f4a9c4e6307d3cc1bb8e586dbe82c06c0ee4e5
KeshabNeupane <keshabneupane061@gmail.com>
2025-05-27 19:11:22 +0545

    pushing all the code

--- django_rest_main/api/views.py
+++ django_rest_main/api/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render 
-from django.http import JsonResponse
+# from django.shortcuts import render 
+# from django.http import JsonResponse
 from students.models import student
-
+from .serializers import studentSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(['GET'])
 def studentsviews(request):
-    students=student.objects.all()
-    studentlist=list(students.values())
-    print(students)
-    return JsonResponse(studentlist, safe=False)
+   if request.method == 'GET':
+    #   get all the data from the student table
+    students = student.objects.all()
+    serializer = studentSerializer(students,many = True)
+    return Response(serializer.data)
