e04f74b343577296fba2b7202f6306926a2bcc83
KeshabNeupane <keshabneupane061@gmail.com>
2025-05-28 08:14:25 +0545

    serializing the single student using endpoint with pk

--- django_rest_main/api/urls.py
+++ django_rest_main/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-  path('students/',views.studentsviews)
+  path('students/',views.studentsviews),
+  path('student/<int:pk>',views.studentDetail)
 ]
--- django_rest_main/api/views.py
+++ django_rest_main/api/views.py
@@ -4,12 +4,31 @@ from students.models import student
 from .serializers import studentSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentsviews(request):
    if request.method == 'GET':
-    #   get all the data from the student table
-    students = student.objects.all()
-    serializer = studentSerializer(students,many = True)
-    return Response(serializer.data)
+      #   get all the data from the student table
+      students = student.objects.all()
+      serializer = studentSerializer(students,many = True)
+      return Response(serializer.data,status=status.HTTP_200_OK)
+   elif request.method == 'POST':
+      serializer = studentSerializer(data=request.data)
+      if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data,status=status.HTTP_201_CREATED)
+      print(serializer)
+      return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)        
+
+@api_view(['GET']) 
+def studentDetail(request,pk):
+   try:
+      students = student.objects.get(pk=pk)
+   except student.DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
+   
+   if request.method == 'GET':
+      Serializer = studentSerializer(students)
+      return Response(Serializer.data,status=status.HTTP_200_OK)
